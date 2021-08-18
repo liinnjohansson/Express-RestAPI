2eb58d9436b691f3258f2bb98ef991691a0c232a
liinnjohansson <93linjoh@skola.boras.se>
2021-08-18 14:48:43 +0200

    added get methods

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
 const express = require('express');
 const app = express();
 
+let shoes = [{
+    id: 0,
+    type: 'heels',
+    colour: 'pink',
+    material: 'leather',
+    size: 38
+}]
+
+app.use(express.json());
+
+app.get('/api/shoes', (req, res) => {
+    res.json(shoes);
+})
+
+app.get('/api/shoes/:id', (req, res) => {
+    const { id } = req.params;
+    const shoe = shoes.find(shoe => shoe.id == id);
+    res.json(shoe);
+})
+
 app.listen(3000, () => {
     console.log('Server is running at http://localhost:3000');
 });
