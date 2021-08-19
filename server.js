53ee9c8f0c747b060185eacc8433c32794809890
liinnjohansson <93linjoh@skola.boras.se>
2021-08-19 10:30:32 +0200

    added delete function

--- server.js
+++ server.js
@@ -1,13 +1,7 @@
 const express = require('express');
 const app = express();
 
-let shoes = [{
-    id: 0,
-    type: 'heels',
-    colour: 'pink',
-    material: 'leather',
-    size: 38
-}]
+let shoes = []
 
 app.use(express.json());
 
@@ -26,6 +20,13 @@ app.post('/api/shoes', (req, res) => {
     shoes.push(shoe);
     res.send('The shoe was successfully added to the storage');
 });
+
+app.delete('/api/shoes/:id', (req, res) => {
+    const { id } = req.params;
+    const i = shoes.findIndex(shoe => shoe.id == id);
+    shoes.splice(i, 1);
+    res.json('REMOVED');
+})
 
 app.listen(3000, () => {
     console.log('Server is running at http://localhost:3000');
