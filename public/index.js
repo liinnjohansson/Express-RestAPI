9042e6ec2c77b8e783a43fe5cbb65cab85615b22
liinnjohansson <93linjoh@skola.boras.se>
2021-08-25 20:18:22 +0200

    can fetch all enpoints

--- public/index.js
+++ public/index.js
@@ -1,7 +1,6 @@
 window.addEventListener("load", main);
 
 function main() {
-  fetchShoes();
   addClickEventOnFetchAllItemsButton();
   addClickEventOnSearchButton();
   addClickEventOnAddButton();
@@ -9,7 +8,7 @@ function main() {
 
 function addClickEventOnFetchAllItemsButton() {
   const btn = document.getElementById("all-btn");
-  btn.addEventListener("click", () => fetchShoes());
+  btn.addEventListener("click", () => fetchAllItems());
 }
 
 function addClickEventOnSearchButton() {
@@ -19,64 +18,188 @@ function addClickEventOnSearchButton() {
 
 function addClickEventOnAddButton() {
   const btn = document.getElementById("add-btn");
-  btn.addEventListener("click", () => addShoe());
+  btn.addEventListener("click", () => displayForm());
+}
+
+function extractItemTemplate() {
+  const template = document.getElementById("shoe-item-template");
+  const listItem = template.content.cloneNode(true);
+  return listItem;
+}
+
+function extractSearchTemplate() {
+  const template = document.getElementById("search-template");
+  const inputField = template.content.cloneNode(true);
+  return inputField;
+}
+
+function extractFormTemplate() {
+  const template = document.getElementById("post-template");
+  const formNode = template.content.cloneNode(true);
+  const form = formNode.getElementById("post-form");
+  return form;
+}
+
+function setSubHeader(text) {
+  subHeader = document.querySelector(".sub-header");
+  subHeader.innerHTML = text;
+}
+
+function clearElements() {
+  const ul = document.getElementById("shoe-list");
+  const searchField = document.getElementById("input-field");
+  const formContainer = document.getElementById("post-form-container");
+  const subHeader = document.querySelector(".sub-header");
+  ul.innerHTML = "";
+  searchField.innerHTML = "";
+  formContainer.innerHTML = "";
+  subHeader.innerHTML = "";
 }
 
 function displaySearchField() {
-  const ul = document.getElementById("shoe-list");
-  const searchField = document.getElementById("input-field");
+  clearElements();
+  const searchFieldContainer = document.getElementById("input-field");
   const template = document.getElementById("search-template");
-  ul.innerHTML = "";
-  searchField.innerHTML = "";
   const inputField = template.content.cloneNode(true);
-  const btn = inputField.getElementById("submit-btn");
-  btn.addEventListener("click", () => fetchOneShoe());
-  searchField.append(inputField);
+  const btn = inputField.getElementById("submit-search-btn");
+  btn.addEventListener("click", () => fetchOneItem());
+  searchFieldContainer.append(inputField);
 }
 
+function displayForm(item) {
+  clearElements();
+  const formContainer = document.getElementById("post-form-container");
+  const form = extractFormTemplate();
+  formContainer.append(form);
+  form.onsubmit = (event) => addOrEditItem(event, item);
+}
 
-async function fetchShoes() {
-  const searchField = document.getElementById("input-field");
+async function fetchAllItems() {
+  clearElements();
+  setSubHeader("All items in storage");
   const ul = document.getElementById("shoe-list");
-  const template = document.getElementById("shoe-item-template");
-  ul.innerHTML = "";
-  searchField.innerHTML = "";
-  
+
   await fetch("/api/shoes")
-  .then((response) => response.json())
-  .then((json) => {
-    const listItem = template.content.cloneNode(true);
-    const subHeader = listItem.querySelector(".sub-header");
-      const paragraf = listItem.getElementById("shoe-item");
-      paragraf.innerText = JSON.stringify(json, null, 4);
-        subHeader.innerHTML = "All shoes in storage";
+    .then((response) => response.json())
+    .then((items) => {
+      for (const item of items) {
+        const listItem = extractItemTemplate();
+        const deleteBtn = listItem.getElementById("delete-btn");
+        const editBtn = listItem.getElementById("edit-btn");
+        const paragraf = listItem.getElementById("shoe-item");
+        paragraf.innerText = JSON.stringify(item, null, 4);
 
-      // removeBtn.addEventListener("click", () => removeTodoItem(todoItem));
-      // editBtn.addEventListener("click", () => displayTodoForm(todoItem));
+        deleteBtn.addEventListener("click", () => deleteItem(item));
+        editBtn.addEventListener("click", () => displayForm(item));
 
-      ul.append(listItem);
+        ul.append(listItem);
+      }
     })
     .catch((err) => console.log("Request Failed", err));
 }
 
-async function fetchOneShoe() {
+async function fetchOneItem() {
+  setSubHeader("Result after search");
   const ul = document.getElementById("shoe-list");
-  const itemTemplate = document.getElementById("shoe-item-template");
-  const searchTemplate = document.getElementById("search-template");
   ul.innerHTML = "";
   const input = document.getElementById("ID");
-  console.log("Inne i fetchOneShoe");
 
   await fetch("/api/shoes/" + input.value)
     .then((response) => response.json())
-    .then((json) => {
-      const listItem = itemTemplate.content.cloneNode(true);
+    .then((item) => {
+      const listItem = extractItemTemplate();
       const paragraf = listItem.getElementById("shoe-item");
-      paragraf.innerText = JSON.stringify(json, null, 4);
+      paragraf.innerText = JSON.stringify(item, null, 4);
+
+      const deleteBtn = listItem.getElementById("delete-btn");
+      const editBtn = listItem.getElementById("edit-btn");
+      deleteBtn.addEventListener("click", () => deleteItem(item));
+      editBtn.addEventListener("click", () => displayForm(item));
 
       ul.append(listItem);
     })
-    .catch((err) => console.log("Request Failed", err));
+    .catch((err) => {
+      console.log("Request Failed", err);
+    });
 
   input.value = "";
+}
+
+async function addOrEditItem(event, item) {
+  event.preventDefault();
+  const inputType = event.target.querySelector("#item-type");
+  const inputColour = event.target.querySelector("#item-colour");
+  const inputMaterial = event.target.querySelector("#item-material");
+  const inputSize = event.target.querySelector("#item-size");
+  const inputQuantity = event.target.querySelector("#item-quantity");
+
+  const newItem = {
+    type: inputType.value,
+    colour: inputColour.value,
+    material: inputMaterial.value,
+    size: inputSize.value,
+    quantity: inputQuantity.value,
+  };
+
+  if (item) {
+    await fetch("/api/shoes/" + item.id, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newItem),
+    })
+      .then((data) => {
+        if (!data.ok) {
+          throw Error(data.status);
+        }
+        return data.json();
+      })
+      .then((newItem) => {
+        console.log(newItem);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  } else {
+    await fetch("/api/shoes", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newItem),
+    })
+      .then((data) => {
+        if (!data.ok) {
+          throw Error(data.status);
+        }
+        return data.json();
+      })
+      .then((newItem) => {
+        console.log(newItem);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+}
+
+async function deleteItem(item) {
+  await fetch("api/shoes/" + item.id, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(item),
+  });
+}
+
+async function editItem(item) {
+  await fetch("api/shoes/" + item.id, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(item),
+  });
 }
