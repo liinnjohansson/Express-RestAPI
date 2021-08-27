c24b8f1733277943af1a7767fbe922abec570618
liinnjohansson <93linjoh@skola.boras.se>
2021-08-27 11:09:00 +0200

    added comments

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const shoesRouter = require('./shoes.router');
 
-//Create instance of express
+//Create express server application
 const app = express();
 
 //Parse incoming data to JSON
 app.use(express.json());
 
-
+//Add resources
 app.use(express.static('public'));
-
 app.use(shoesRouter);
 
+//Start the server
 app.listen(3000, () => {
     console.log('Server is running at http://localhost:3000');
 });
--- shoes.router.js
+++ shoes.router.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const { getShoes, getOneShoe, addShoe, editShoe, deleteShoe} = require('./shoes.controllers');
 
+//Create router object
 const router = express.Router();
 
+//Define router endpoints
 router.get('/api/shoes', getShoes);
 router.get('/api/shoes/:id', getOneShoe);
 router.post('/api/shoes', addShoe);
 router.put('/api/shoes/:id', editShoe)
 router.delete('/api/shoes/:id', deleteShoe)
 
+//Export router object
 module.exports = router;
