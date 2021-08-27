c4a8c3431a089a75c71514c3cc05be3b2e1fbc7d
liinnjohansson <93linjoh@skola.boras.se>
2021-08-27 11:03:50 +0200

    removed unnecessary spaces

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const shoesRouter = require('./shoes.router');
+
+//Create instance of express
 const app = express();
 
+//Parse incoming data to JSON
 app.use(express.json());
+
 
 app.use(express.static('public'));
 
