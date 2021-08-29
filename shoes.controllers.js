b8ae368494f57c74a85201739bb0673cc518cb83
liinnjohansson <93linjoh@skola.boras.se>
2021-08-29 19:41:30 +0200

    all functions works

--- shoes.controllers.js
+++ shoes.controllers.js
@@ -1,83 +1,115 @@
-const { json } = require('express');
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 const { v4: uuidv4 } = require("uuid");
+const status = require("./shoes.statuscode.handler");
 
-let shoesPath = path.resolve(__dirname, 'shoes.json');
+//Define path to jSON-file
+let shoesPath = path.resolve(__dirname, "shoes.json");
 
-const readJsonFile = () => {
-    //Gets the local path for project and file
-    let shoesPath = path.resolve(__dirname, 'shoes.json');
-    
-    //Gets the buffer data from file
-    let shoesData = fs.readFileSync(shoesPath);
-    
-    //Parse the buffer data to json
-    let shoes = JSON.parse(shoesData);
-
-    return shoes;
-}
-
+//Gets all shoes
 const getShoes = (req, res) => {
-    res.status(200).json(readJsonFile());
-}
+  fs.readFile(shoesPath, "utf8", (err, data) => {
+    if (err) {
+      status.readingFileError(res, err);
+    } else {
+      if (data) {
+        const shoes = JSON.parse(data);
+        if(shoes.length > 0) status.success(res, shoes);
+        else status.shoesNotFound(res);
+      } else status.shoesNotFound(res);
+    }
+  });
+};
 
+//Gets one specific shoe depending on requested id
 const getOneShoe = (req, res) => {
-    const { id } = req.params;
-    const shoe = readJsonFile().find(shoe => shoe.id == id);
-    if(shoe != null) res.status(200).json(shoe);
-    else res.status(404).json('Does not exist');
-}
+  const { id } = req.params;
+  fs.readFile(shoesPath, "utf8", (err, data) => {
+    if (err) {
+      status.readingFileError(res, err);
+    } else {
+      if (data) {
+        const shoes = JSON.parse(data);
+        const shoe = shoes.find((shoe) => shoe.id == id);
+        if (shoe) status.success(res, shoe);
+        else status.shoeNotFound(res);
+      } else status.shoesNotFound(res);
+    }
+  });
+};
 
+//Adds shoe by request
 const addShoe = (req, res) => {
-    const shoe = {...req.body, id: uuidv4()};
-    const newList = readJsonFile();
-    newList.push(shoe);
-    fs.writeFile(shoesPath, JSON.stringify(newList, null, 4), (err) => {
-                if(err){
-                    console.log(`Error accured while trying to write to file: ${err}`);
-                }
-                else res.status(200).json(shoe);
-            });
-}
+  const shoe = { ...req.body, id: uuidv4() };
 
+  fs.readFile(shoesPath, "utf8", (err, data) => {
+    if (err) {
+      status.readingFileError(res, err);
+    } else {
+      if (data) {
+        const shoes = JSON.parse(data);
+        shoes.push(shoe);
+        fs.writeFile(shoesPath, JSON.stringify(shoes, null, 4), (err) => {
+          if (err) {
+            status.writingFileError(res, err);
+          } else status.success(res, shoe);
+        });
+      } else status.addBadRequest(res);
+    }
+  });
+};
+
+//Edits shoe depending on id from request
 const editShoe = (req, res) => {
-    const { id } = req.params;
-    const newList = readJsonFile();
-    const shoe = newList.find(shoe => shoe.id == id);
-    if(shoe != null){
-        Object.assign(shoe, req.body);
-    fs.writeFile(shoesPath, JSON.stringify(newList, null, 4), (err) => {
-                if(err){
-                    console.log(`Error accured while trying to write to file: ${err}`);
-                }
-                else res.status(200).json(shoe);
-            });
-    } 
-    else res.status(404).json('Does not exist');
-}
+  const { id } = req.params;
+  fs.readFile(shoesPath, "utf8", (err, data) => {
+    if (err) {
+      status.readingFileError(res, err);
+    } else {
+      if (data) {
+        const shoes = JSON.parse(data);
+        const shoe = shoes.find((shoe) => shoe.id == id);
+        if (shoe) {
+          Object.assign(shoe, req.body);
+          fs.writeFile(shoesPath, JSON.stringify(shoes, null, 4), (err) => {
+            if (err) {
+              status.writingFileError(res, err);
+            } else status.success(res, shoe);
+          });
+        } else status.shoeNotFound(res);
+      }
+      else status.shoesNotFound(res);
+    }
+  });
+};
 
+//Deletes shoe depending on id from request
 const deleteShoe = (req, res) => {
-    const { id } = req.params;
-    const newList = readJsonFile();
-    const i = newList.findIndex(shoe => shoe.id == id);
-    if(i != -1) {
-        newList.splice(i, 1);
-            fs.writeFile(shoesPath, JSON.stringify(newList, null, 4), (err) => {
-                if(err){
-                    console.log(`Error accured while trying to write to file: ${err}`);
-                }
-                else res.status(200).json('Confirm: Item was removed');
-            });
-    } 
-    else res.status(404).json('Error: Can not find item to remove');
-}
+  const { id } = req.params;
+  fs.readFile(shoesPath, "utf8", (err, data) => {
+    if (err) {
+      status.readingFileError(res, err);
+    } else {
+      if (data) {
+        const shoes = JSON.parse(data);
+        const i = shoes.findIndex((shoe) => shoe.id == id);
+        if (i != -1) {
+          shoes.splice(i, 1);
+          fs.writeFile(shoesPath, JSON.stringify(shoes, null, 4), (err) => {
+            if (err) {
+              status.writingFileError(res, err);
+            } else status.deleteSuccess(res);
+          });
+        } else status.shoeNotFound(res);
+      } else status.shoesNotFound(res);
+    }
+  });
+};
 
 module.exports = {
-    getShoes,
-    getOneShoe,
-    addShoe,
-    editShoe,
-    deleteShoe
-}
+  getShoes,
+  getOneShoe,
+  addShoe,
+  editShoe,
+  deleteShoe,
+};
